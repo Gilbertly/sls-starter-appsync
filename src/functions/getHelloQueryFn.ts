fdad0a97a450dcd746fb2c70db1bc7b91a6298b8
Gilbertly <gilbert@qubitping.com>
2019-10-28 16:17:58 +0300

    refactor: rename api folder & implement gql resources

---
+++ src/functions/getHelloQueryFn.ts
@@ -0,0 +1,6 @@
+import { Context } from 'aws-lambda';
+
+exports.handler = async (event: any, context: Context): Promise<string> => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return 'Hello!'
+}
