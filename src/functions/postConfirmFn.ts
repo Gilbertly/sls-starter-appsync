de1f6029a0efaff1509d0ca407aae6fabfbb43cd
Gilbertly <gilbert@qubitping.com>
2020-05-21 11:13:47 +0300

    chore: update userId attr to link duplicate user accounts

--- src/functions/postConfirmFn.ts
+++ src/functions/postConfirmFn.ts
@@ -1,5 +1,6 @@
 import * as CognitoISP from 'aws-sdk/clients/cognitoidentityserviceprovider';
 import * as Sentry from '@sentry/node';
+import * as crypto from 'crypto';
 import { Context, CognitoUserPoolTriggerEvent } from 'aws-lambda';
 
 const cognitoClient = new CognitoISP();
@@ -12,6 +13,9 @@ exports.handler = async (
   context.callbackWaitsForEmptyEventLoop = false;
 
   try {
+    const userEmail = event.request.userAttributes?.email;
+    const userId = crypto.createHash('md5').update(userEmail).digest('hex');
+
     await cognitoClient
       .adminAddUserToGroup({
         GroupName: 'BASIC',
@@ -19,7 +23,18 @@ exports.handler = async (
         Username: event.userName || '',
       })
       .promise();
-    console.log(`Added user '${event.userName}' to group 'BASIC'.`);
+
+    await cognitoClient
+      .adminUpdateUserAttributes({
+        UserPoolId: event.userPoolId || '',
+        Username: event.userName || '',
+        UserAttributes: [{ Name: 'custom:userId', Value: userId }],
+      })
+      .promise();
+
+    console.log(
+      `Added user to group 'BASIC' and updated attribute 'custom:userId'.`,
+    );
   } catch (error) {
     Sentry.captureException(error);
   }
