c22ad52ec1122b8c2b7f20cf5e53df29d0f13a77
Gilbertly <gilbert@qubitping.com>
2020-05-23 12:46:05 +0300

    chore: implement better user-owned-item querying

--- src/models/tableItems.ts
+++ src/models/tableItems.ts
@@ -1,44 +1,73 @@
 import * as DynamoDB from 'aws-sdk/clients/dynamodb';
 
-interface AddItem {
-  tableName: string;
-  itemId: string;
-  itemName: string;
-}
-
-interface GetItem {
-  tableName: string;
-  itemId: string;
-}
-
 export const getItem = async (
   documentClient: DynamoDB.DocumentClient,
-  payload: GetItem,
+  payload: {
+    userId: string;
+    tableName: string;
+    itemId: string;
+  },
 ) => {
-  const data = await documentClient
+  const response = await documentClient
     .query({
       TableName: payload.tableName,
-      KeyConditionExpression: 'itemId = :itemId',
+      KeyConditionExpression: 'itemId = :itemId and userId = :userId',
       ExpressionAttributeValues: {
+        ':userId': payload.userId,
         ':itemId': payload.itemId,
       },
     })
     .promise();
-  if (data.Items) return data.Items[0];
-  return data.Items;
+  if (response.Items) return response.Items[0];
+  return response.Items;
+};
+
+export const getItems = async (
+  documentClient: DynamoDB.DocumentClient,
+  payload: {
+    userId: string;
+    tableName: string;
+    indexName: string;
+  },
+) => {
+  const response = await documentClient
+    .query({
+      TableName: payload.tableName,
+      IndexName: payload.indexName,
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': payload.userId,
+      },
+    })
+    .promise();
+  return response.Items;
 };
 
 export const addItem = async (
   documentClient: DynamoDB.DocumentClient,
-  payload: AddItem,
+  payload: {
+    userId: string;
+    tableName: string;
+    itemId: string;
+    itemName: string;
+  },
 ) => {
-  return await documentClient
-    .put({
-      TableName: payload.tableName,
-      Item: {
-        itemId: payload.itemId,
-        itemName: payload.itemName,
-      },
-    })
-    .promise();
+  let response;
+  try {
+    console.log('add item payload:', JSON.stringify(payload));
+    response = await documentClient
+      .put({
+        TableName: payload.tableName,
+        Item: {
+          userId: payload.userId,
+          itemId: payload.itemId,
+          itemName: payload.itemName,
+        },
+      })
+      .promise()
+      .then(res => res);
+  } catch (error) {
+    console.error('error adding item:', JSON.stringify(error));
+  }
+  return response;
 };
