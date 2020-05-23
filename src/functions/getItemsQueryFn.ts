8dd60c6e93469848a1d667c8ac92ed4b572c0af6
Gilbertly <gilbert@qubitping.com>
2020-05-23 12:44:15 +0300

    feat: add getItems lambda function

---
+++ src/functions/getItemsQueryFn.ts
@@ -0,0 +1,29 @@
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
+import * as Sentry from '@sentry/node';
+import { Context } from 'aws-lambda';
+import { AppSyncLambdaEvent } from '../utils/appsyncLambda';
+import { captureException } from '../utils/sentryLogs';
+import { getItems } from '../models/tableItems';
+
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+let documentClient: DynamoDB.DocumentClient;
+
+exports.handler = async (event: AppSyncLambdaEvent, context: Context) => {
+  if (!documentClient) documentClient = new DynamoDB.DocumentClient();
+  context.callbackWaitsForEmptyEventLoop = false;
+  let response;
+
+  try {
+    console.log(`Processing getItems ...`);
+    response = await getItems(documentClient, {
+      userId: event.identity.claims['custom:userId'],
+      tableName: process.env.TABLE_ITEMS || '',
+      indexName: process.env.INDEX_NAME || '',
+    });
+    console.log(`Successfully queried items.`);
+  } catch (error) {
+    captureException(context, error);
+    console.error('Error getting items:', JSON.stringify(error));
+  }
+  return response;
+};
