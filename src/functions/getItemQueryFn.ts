5288c02bbc584a5ca1b0555613b031109eb52368
Gilbertly <gilbert@qubitping.com>
2020-05-23 12:42:16 +0300

    chore: improve get item config

--- src/functions/getItemQueryFn.ts
+++ src/functions/getItemQueryFn.ts
@@ -2,25 +2,27 @@ import * as DynamoDB from 'aws-sdk/clients/dynamodb';
 import * as Sentry from '@sentry/node';
 import { Context } from 'aws-lambda';
 import { AppSyncLambdaEvent } from '../utils/appsyncLambda';
+import { captureException } from '../utils/sentryLogs';
 import { getItem } from '../models/tableItems';
 
 Sentry.init({ dsn: process.env.SENTRY_DSN });
-const documentClient = new DynamoDB.DocumentClient();
+let documentClient: DynamoDB.DocumentClient;
 
 exports.handler = async (event: AppSyncLambdaEvent, context: Context) => {
+  if (!documentClient) documentClient = new DynamoDB.DocumentClient();
   context.callbackWaitsForEmptyEventLoop = false;
   let response;
 
   try {
-    console.log(`Processing event ...`);
+    console.log(`Processing getItem ...`);
     response = await getItem(documentClient, {
+      userId: event.identity.claims['custom:userId'],
       tableName: process.env.TABLE_ITEMS || '',
       itemId: event.arguments.itemId || '',
     });
-    console.log(JSON.stringify(response));
+    console.log(`Successfully queried itemId: ${event.arguments.itemId}`);
   } catch (error) {
-    Sentry.captureException(error);
+    captureException(context, error);
   }
-
   return response;
 };
