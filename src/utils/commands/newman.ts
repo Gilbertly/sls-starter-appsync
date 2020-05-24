599c2fd4784af91c7976ac7b4144f2a72893e000
Gilbertly <gilbert@qubitping.com>
2020-05-24 11:09:13 +0300

    chore: add environment id config

--- src/utils/commands/newman.ts
+++ src/utils/commands/newman.ts
@@ -3,23 +3,27 @@ import * as ora from 'ora';
 import * as newman from 'newman';
 
 const spinner = ora({ spinner: 'bouncingBar' });
+const baseUrl = 'https://api.getpostman.com'
 const apiKey = process.env.POSTMAN_API_KEY || '';
 const collectionId = process.env.POSTMAN_COLLECTION_ID || '';
+const envDevId = process.env.POSTMAN_ENV_DEV_ID || '';
 
 export const build = (): yargs.CommandModule => {
   const runCmd = async (args: any) => {
-    spinner.info(`Running collection ...`);
-
-    newman.run(
-      {
-        collection: `https://api.getpostman.com/collections/${collectionId}?apikey=${apiKey}`,
+    newman
+      .run({
+        collection: `${baseUrl}/collections/${collectionId}?apikey=${apiKey}`,
+        environment: `${baseUrl}/environments/${envDevId}?apikey=${apiKey}`,
         reporters: 'cli',
-      },
-      (error: any) => {
-        if (error) throw error;
-        spinner.succeed(`Successfully ran collection!`);
-      },
-    );
+      })
+      .on('start', (error, summary) => {
+        if (error || summary.error) throw error;
+        spinner.info(`Running collection ...`);
+      })
+      .on('done', (error, summary) => {
+        if (error || summary.error) throw error;
+        spinner.succeed(`Completed collection run!`);
+      });
   };
 
   return {
